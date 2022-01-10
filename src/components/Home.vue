b2e8086bf097f85f88f112a557ebeb74aaa77a72
Adejuwon Oshadipe <bibire95@gmail.com>
2022-01-10 19:07:28 +0100

    Home css defined

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,31 @@
 <template>
-  <div>
-    <div>
+  <div class="contain">
+    <div class="head">
       <h1>Home</h1>
       <router-link to="/history"><button>History</button></router-link>
     </div>
-
-    <form @submit.prevent="jumps(this.x1, this.v1, this.x2, this.v2)">
-      <label name="x1">X1</label>
-      <input type="number" name="x1" v-model="x1">
-      <label name="v1">V1</label>
-      <input type="number" name="v1" v-model="v1">
-      <label name="x2">X2</label>
-      <input type="number" name="x2" v-model="x2">
-      <label name="v2">V2</label>
-      <input type="number" name="v2" v-model="v2">
-      <input type="submit">
-    </form>
+    <div class="frmctrl">
+        <form @submit.prevent="jumps(this.x1, this.v1, this.x2, this.v2)">
+          <label name="x1">X1</label>
+          <input type="number" name="x1" v-model="x1">
+          <label name="v1">V1</label>
+          <input type="number" name="v1" v-model="v1">
+          <label name="x2">X2</label>
+          <input type="number" name="x2" v-model="x2">
+          <label name="v2">V2</label>
+          <input type="number" name="v2" v-model="v2">
+          <input type="submit">
+        </form>
+    </div>
     <div>
       <h3>Results</h3>
       <div>
-        <p> x1: {{result.x1}}</p>
-        <p>v1: {{result.v1}}</p>
-        <p>x2: {{result.x2}}</p>
-        <p>v2: {{result.v2}}</p>
+        <p>X1: {{result.x1}}</p>
+        <p>V1: {{result.v1}}</p>
+        <p>X2: {{result.x2}}</p>
+        <p>V2: {{result.v2}}</p>
         <p>Outcome: {{result.returnVal}}</p>
-      </div>
-        
+      </div>   
     </div>
   </div>
 
@@ -60,5 +60,56 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.contain{
+  display: flex;
+  flex-direction: column;
+  .head{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    a{
+      padding: 75px;
+      
+    }
+  }
+  .frmctrl{
+    //width: 400px;
+    background: white;
+    padding: 20px;
+    label{
+        font-size: 16px;
+        font-weight: 600;
+        padding-right: 3px;
+    }
+    input{
+        margin: 8px;
+        height: 30px;
+        background:  rgba(158, 226, 165, 0.73);
+        border: none;
+        border-radius: 3px;
+        outline: none;
+        font-size: 16px;  
+    }
+       input[type="submit"]{
+        height: 30px;
+        width: 130px;
+        background: #1466DB;
+        border-radius: 3px;
+        color: white;
+        font-size: 16px;
+        margin-right: 10px;
+    }
+  }
+  button{
+        position: absolute;
+        right: 150px;
+        width: 88px;
+        height: 30px;
+        background: #1466DB;
+        color: white;
+        border: none;
+        border-radius: 3px;
+  }
+}
 
 </style>
